c3f35abd2be35a5e798872bcd519460ae5d20ac0
Ruben Ahlin <Ruben.Ahlin@Chasacademy.se>
2024-11-20 15:37:33 +0100

    Lesson 4.4

--- src/App.vue
+++ src/App.vue
@@ -7,21 +7,27 @@
 
     <div v-show="false">Hide this</div>
 
-    <PageViewer
+    <!-- <PageViewer
         v-if="pages.length > 0"
         :page="pages[activePage]"
-    ></PageViewer>
+    ></PageViewer> -->
+
+    <create-page
+        :page-created="pageCreated"
+    ></create-page>
 </template>
 
 <script>
 import Navbar from './components/Navbar.vue'
 import PageViewer from './components/PageViewer.vue'
+import CreatePage from './components/CreatePage.vue';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 
 export default{
     components: {
         Navbar,
         PageViewer,
+        CreatePage,
     },
     created() {
         this.getPages()
@@ -37,6 +43,9 @@ export default{
             let res = await fetch('pages.json')
             let data = await res.json()
             this.pages = data;
+        },
+        pageCreated(pageObj){
+            console.log(pageObj);
         }
     }
 }
---
+++ src/components/CreatePage.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="container mb-3">
+        <form action="">
+            <div class="mb-3">
+                <label for="" class="form-label">
+                    Page Title
+                </label>
+                <input 
+                    v-model="pageTitle"
+                    type="text" 
+                    class="form-control"
+                />
+            </div>
+            <div class="mb-3">
+                <label for="" class="form-label">
+                    Content
+                </label>
+                <textarea
+                v-model="content"
+                rows="5" 
+                name="" 
+                id=""
+                class="form-control"
+                ></textarea>
+            </div>
+            <div class="mb-3">
+                <button 
+                    class="btn btn-primary"
+                    @click.prevent="pageCreated({pageTitle, content})"
+                >Create Page</button>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['pageCreated'],
+        data() {
+            return {
+                pageTitle: '',
+                content: ''
+            }
+        }
+    }
+</script>
